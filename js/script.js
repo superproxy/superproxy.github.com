d3d39bf3c506ad8dd994fded0dfa412bf898fdcb
superproxy <sharpcoder@qq.com>
2015-07-11 11:17:42 +0800

    添加nginx实践

--- js/script.js
+++ js/script.js
@@ -119,3 +119,21 @@
 
 
 <script type="text/javascript" src="http://v3.jiathis.com/code/jia.js" charset="utf-8"></script>
+
+
+
+function searchFormHelper(options){
+  /* jshint validthis: true */
+  options = options || {};
+
+  var config = this.config;
+  var className = options.class || 'search-form';
+  var text = options.hasOwnProperty('text') ? options.text : 'Search';
+  var button = options.button;
+
+  return '<form action="//google.com/search" method="get" accept-charset="UTF-8" class="' + className + '">' +
+    '<input type="search" name="q" results="0" class="' + className + '-input"' + (text ? ' placeholder="' + text + '"' : '') + '>' +
+    (button ? '<button type="submit" class="' + className + '-submit">' + (typeof button === 'string' ? button : text) + '</button>' : '') +
+    '<input type="hidden" name="sitesearch" value="' + config.url + '">' +
+    '</form>';
+}
